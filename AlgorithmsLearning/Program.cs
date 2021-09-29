44826e2133636053b137fa08de9add1a8968a0f1
Raixther <g.a.z@bk.ru>
2021-09-29 11:12:22 +0300

    Add project files.

---
+++ AlgorithmsLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimplicityCheckingStuff.IsSimple(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
---
+++ AlgorithmsLearning/SimplicityCheckingStuff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public static class SimplicityCheckingStuff
+    {
+       private  static int d = 0;
+
+       private static int i = 2;
+       static public void IsSimple(int n)
+        {
+       
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (d == 0)
+            {
+                Console.WriteLine("Простое");
+            }
+            else
+            {
+                Console.WriteLine("Не Простое");
+            }
+        }
+      }
+    }
+
